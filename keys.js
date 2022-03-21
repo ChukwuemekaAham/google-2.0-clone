9d92beb1fef020a9bab6a396853a5217186269d9
Andrew.Aham <ahamchukwuemeka2@gmail.com>
2022-03-21 15:35:18 +0100

    first commit

---
+++ keys.js
@@ -0,0 +1,11 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+
+export const API_KEY = "AIzaSyATYu-RTD7WTbkMBlKMBGyoINUmoQgACIQ";
+
+// Context Key - https://cse.google.com/cse/create/new
+
+{
+  /* <script async src="https://cse.google.com/cse.js?cx=ccac24589e9d382a7"></script> */
+}
+
+export const CONTEXT_KEY = "ccac24589e9d382a7";
